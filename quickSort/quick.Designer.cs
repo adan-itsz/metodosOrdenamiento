ab6502a7d65629f9a38269f49256dd9c458816d0
Adan De Santiago <adan1995a@gmail.com>
2015-10-15 21:25:38 -0500

    menu inicio, falta agregar todos los metodos de ordenamiento

---
+++ quickSort/quick.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace quickSort
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxCantidadElementos = new System.Windows.Forms.TextBox();
+            this.btnLlenar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnQuickSort = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(199, 123);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+            this.btnAceptar.TabIndex = 11;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Longitud de arreglo";
+            // 
+            // txtBoxCantidadElementos
+            // 
+            this.txtBoxCantidadElementos.Location = new System.Drawing.Point(41, 124);
+            this.txtBoxCantidadElementos.Name = "txtBoxCantidadElementos";
+            this.txtBoxCantidadElementos.Size = new System.Drawing.Size(131, 22);
+            this.txtBoxCantidadElementos.TabIndex = 9;
+            // 
+            // btnLlenar
+            // 
+            this.btnLlenar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLlenar.Location = new System.Drawing.Point(41, 178);
+            this.btnLlenar.Name = "btnLlenar";
+            this.btnLlenar.Size = new System.Drawing.Size(151, 29);
+            this.btnLlenar.TabIndex = 12;
+            this.btnLlenar.Text = "Llenar random";
+            this.btnLlenar.UseVisualStyleBackColor = true;
+            this.btnLlenar.Click += new System.EventHandler(this.btnLlenar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(296, 36);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(511, 367);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 249);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(263, 20);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Elige metodo de ordenamiento";
+            // 
+            // btnQuickSort
+            // 
+            this.btnQuickSort.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuickSort.Location = new System.Drawing.Point(32, 294);
+            this.btnQuickSort.Name = "btnQuickSort";
+            this.btnQuickSort.Size = new System.Drawing.Size(151, 29);
+            this.btnQuickSort.TabIndex = 15;
+            this.btnQuickSort.Text = "QuickSort";
+            this.btnQuickSort.UseVisualStyleBackColor = true;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 466);
+            this.Controls.Add(this.btnQuickSort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnLlenar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxCantidadElementos);
+            this.Name = "frmPrincipal";
+            this.Text = "Métodos de ordenamiento";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxCantidadElementos;
+        private System.Windows.Forms.Button btnLlenar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnQuickSort;
+
+    }
+}
+
---
+++ quickSort/quick.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quickSort
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+        int[] array;
+        int longitudArreglo;
+        int num;
+        Random rand = new Random();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Add("", "");
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            try{
+            longitudArreglo = Convert.ToInt32(txtBoxCantidadElementos.Text);
+            array = new int[longitudArreglo];
+            for (int i = 0; i < longitudArreglo - 1; i++)
+                {
+                    dataGridView1.Rows.Add();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("llenar correctamente");
+            }
+        }
+
+        private void btnLlenar_Click(object sender, EventArgs e)
+        {
+            int j = 0;
+            while (j < array.Length)
+            {
+                num = rand.Next(0, 200);
+                dataGridView1[0, j].Value = num;
+                array[j] = num;
+                j++;
+            }
+            MessageBox.Show("arreglo lleno");
+        }
+    }
+}
